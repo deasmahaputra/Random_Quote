edac35bd25fe937820a209b66b7bfd6685edde64
deas maha putra <deasmahaputra@gmail.com>
2021-08-07 12:10:10 +0700

    add event and state

---
+++ lib/bloc/QuoteEvent.dart
@@ -0,0 +1,14 @@
+import 'package:equatable/equatable.dart';
+
+abstract class QuoteEvent extends Equatable{
+  const QuoteEvent();
+}
+
+class FetchQuote extends QuoteEvent {
+
+  const FetchQuote();
+  
+  @override
+  List<Object> get props => [];
+
+}
---
+++ lib/bloc/QuoteState.dart
@@ -0,0 +1,27 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:quote_app/models/quote.dart';
+
+abstract class QuoteState extends Equatable {
+
+  const QuoteState();
+
+  @override
+  List<Object> get props => [];
+
+}
+
+class QuoteEmpty extends QuoteState{}
+
+class QuoteLoading extends QuoteState{}
+
+class QuoteLoaded extends QuoteState {
+  final Quote quote;
+
+  const QuoteLoaded({@required this.quote}) : assert(quote != null);
+
+  @override
+  List<Object> get props => [quote];
+}
+
+class QuoteError extends QuoteState{}
